e7e26fa3addea921f387c2ff682b5486de9117d1
vmishkovets <v.mishkovets@belitsoft.com>
2022-12-14 11:41:23 +0300

    connect to mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import {Module} from "@nestjs/common";
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
   controllers: [
     AppController
   ],
-  providers: [AppService]
+  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb+srv://ADMIN:EHhiHeadSM6taShi@cluster0.ab4ywu3.mongodb.net/?retryWrites=true&w=majority')],
 })
 
 export class AppModule {
