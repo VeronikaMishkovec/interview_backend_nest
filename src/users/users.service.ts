90bc9fc9b7b15569617b4b3514199b4f346d336c
vmishkovets <v.mishkovets@belitsoft.com>
2023-01-24 16:48:56 +0300

    feat: create user and registration request

---
+++ src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
+import {User, UserDocument} from "./users.schema";
+import {CreateUserDto} from "../dto/create-user.dto";
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+  }
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userModel.create({...dto})
+    return user
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.userModel.findOne({ email })
+    return user
+  }
+}
